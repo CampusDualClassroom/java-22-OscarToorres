3931e1e899487ddc15e688d47a6b55ad24c9c010
Oscar Torres Rodriguez <torresrdriguezoscar@gmail.com>
2024-11-21 14:49:16 +0100

    Ejercicio 22 completado

--- src/main/java/com/campusdual/classroom/Exercise22.java
+++ src/main/java/com/campusdual/classroom/Exercise22.java
@@ -8,13 +8,38 @@ public class Exercise22 {
 
     public static List<String> createArrayList() {
 
+        List<String> stringList = new ArrayList<>();
+
+        stringList.add("GKFFD");
+        stringList.add("TNANA");
+        stringList.add("MPMSL");
+        stringList.add("PSWME");
+        stringList.add("LZMLF");
+        stringList.add("JYEBV");
+        stringList.add("YELNT");
+        stringList.add("JSNKR");
+        stringList.add("JFESF");
+        stringList.add("TMJLL");
+
+        return stringList;
     }
 
     public static void printElementsAndIndex(List<String> customList) {
-
+        for (String str : customList){
+            System.out.println(str);
+        }
     }
 
     public static boolean addElementToList(List<String> customList, String element) {
+
+        boolean comprobacion = false;
+        try {
+            customList.add(element);
+            comprobacion = true;
+        } catch (Exception e){
+        }
+
+        return comprobacion;
 
     }
 
